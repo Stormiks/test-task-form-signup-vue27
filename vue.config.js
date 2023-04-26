83edae7165695d9f75fedbdc3b07560c3c42c8f5
Skylinker <skylinkertu33@gmail.com>
2023-04-26 18:29:20 +0700

    feat(config): Adding a CSS-preprocessor plugin "LESS"

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
+const path = require("path");
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "less",
+      patterns: [path.join(__dirname, "./src/assets/less/_variables.less")],
+    },
+  },
 });
